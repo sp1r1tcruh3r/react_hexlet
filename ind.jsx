7dbeb755c36f6ad85fe7c44cf544c6c3737caf59
sp1r1tcruh3r <48630956+sp1r1tcruh3r@users.noreply.github.com>
2019-10-10 09:46:47 +0300

    Add files via upload

---
+++ ind.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Card from './Card'
+// BEGIN (write your solution here)
+ReactDOM.render(
+  <Card />,
+  document.getElementById('container'),
+);
+// END
